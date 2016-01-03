e0a324fcfa3320b02ec21e70ac1be4692dfe832e
hgdphg <hgdphg@gmail.com>
2016-01-04 02:17:33 +0700

    add Project and add webpack(basic)

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    progress: true,
+    historyApiFallback: true,
+    stats: 'errors-only',
+    port: process.env.PORT,
+    host: process.env.HOST
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'My app'
+    })
+  ]
+};
